68e031faeaf8939b01747879e7c4f9109dd04019
qyzzy <42561599+qyzzy@users.noreply.github.com>
2022-06-26 10:18:42 +0800

    update rabbitMQ

--- initialize/rabbitmq.go
+++ initialize/rabbitmq.go
@@ -4,8 +4,14 @@ import (
 	"fmt"
 	"github.com/streadway/amqp"
 	"gostars/global"
+	"gostars/middlewares/rabbitmq"
 	"gostars/utils"
 	"log"
+)
+
+var (
+	RmqLikeAdd *rabbitmq.LikeMQ
+	RmqLikeDel *rabbitmq.LikeMQ
 )
 
 func init() {
@@ -21,4 +27,14 @@ func init() {
 	if err != nil {
 		log.Println(err)
 	}
+
+	InitLikeRabbitMQ()
+}
+
+func InitLikeRabbitMQ() {
+	RmqLikeAdd = rabbitmq.NewLikeRabbitMQ("like_add")
+	go RmqLikeAdd.Consumer()
+
+	RmqLikeDel = rabbitmq.NewLikeRabbitMQ("like_del")
+	go RmqLikeDel.Consumer()
 }
