61b6738c990e13e45e2598af446acfc6c4adaff5
qyzzy <42561599+qyzzy@users.noreply.github.com>
2022-06-27 18:06:29 +0800

    update favoriteAction api

--- api/v1/web/likeArticleRestApi.go
+++ api/v1/web/likeArticleRestApi.go
@@ -1,7 +1,21 @@
 package web
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"gostars/utils/code"
+	"net/http"
+	"strconv"
+)
 
 func (likeArticleApi *LikeArticleApi) FavoriteAction(c *gin.Context) {
+	articleID, _ := strconv.Atoi(c.Param("id"))
+	userID, _ := strconv.Atoi(c.Query("id"))
+	actionType, _ := strconv.Atoi(c.Query("actiontype"))
 
+	errCode := webFavoriteService.FavoriteAction(userID, articleID, actionType)
+
+	c.JSON(http.StatusOK, gin.H{
+		"status":  errCode,
+		"message": code.GetErrMsg(errCode),
+	})
 }
