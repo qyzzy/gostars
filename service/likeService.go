f9f5806dfdd6bf9746b82889f029a5bb383023ff
qyzzy <42561599+qyzzy@users.noreply.github.com>
2022-06-27 18:06:50 +0800

    update &userList

--- service/likeService.go
+++ service/likeService.go
@@ -63,7 +63,7 @@ func (likeService *LikeService) GetLikeArticleList(userID int) ([]int, int) {
 	var likeArticleList []int
 	err := global.GDb.Table(models.LikeTableName()).Model(&models.Like{}).
 		Where(map[string]interface{}{"user_id": userID, "cancel": utils.IsLike}).
-		Pluck("article_id", likeArticleList).Error
+		Pluck("article_id", &likeArticleList).Error
 	if err != nil {
 		if err.Error() == "record not found" {
 			return likeArticleList, code.ErrorDataNotFound
@@ -76,6 +76,7 @@ func (likeService *LikeService) GetLikeArticleList(userID int) ([]int, int) {
 /**
 	Redis Part
 **/
+// likeUserID
 func (likeService *LikeService) ExistsRedisLikeUserID(strUserID string) int {
 	res, err := global.GRedisGroup[1].Exists(context.Background(), strUserID).Result()
 	if err != nil {
@@ -108,9 +109,65 @@ func (likeService *LikeService) ExpireRedisLikeUserID(strUserID string) int {
 		context.Background(),
 		strUserID,
 		time.Duration(utils.Week)*time.Second,
-	)
+	).Err()
 	if err != nil {
 		return code.ErrorRedisExpireFailed
+	}
+	return code.SUCCESS
+}
+
+func (likeService *LikeService) SRemRedisLikeUserID(strUserID string, articleID int) int {
+	err := global.GRedisGroup[1].SRem(context.Background(), strUserID, articleID).Err()
+	if err != nil {
+		return code.ErrorRedisDeleteFailed
+	}
+	return code.SUCCESS
+}
+
+// likeArticleID
+func (likeService *LikeService) ExistsRedisLikeArticleID(strArticleID string) int {
+	res, err := global.GRedisGroup[2].Exists(context.Background(), strArticleID).Result()
+	if err != nil {
+		return code.ERROR
+	}
+	if res == 0 {
+		return code.ErrorRedisKeyNotExist
+	}
+	return code.SUCCESS
+}
+
+func (likeService *LikeService) SAddRedisLikeArticleID(strArticleID string, userID int) int {
+	_, err := global.GRedisGroup[2].SAdd(context.Background(), strArticleID, userID).Result()
+	if err != nil {
+		return code.ErrorRedisSaveFailed
+	}
+	return code.SUCCESS
+}
+
+func (likeService *LikeService) DelRedisLikeArticleID(strArticleID string) int {
+	err := global.GRedisGroup[2].Del(context.Background(), strArticleID).Err()
+	if err != nil {
+		return code.ErrorRedisDeleteFailed
+	}
+	return code.SUCCESS
+}
+
+func (likeService *LikeService) ExpireRedisLikeArticleID(strArticleID string) int {
+	err := global.GRedisGroup[2].Expire(
+		context.Background(),
+		strArticleID,
+		time.Duration(utils.Week)*time.Second,
+	).Err()
+	if err != nil {
+		return code.ErrorRedisExpireFailed
+	}
+	return code.SUCCESS
+}
+
+func (likeService *LikeService) SRemRedisLikeArticleID(strArticleID string, userID int) int {
+	err := global.GRedisGroup[2].SRem(context.Background(), strArticleID, userID).Err()
+	if err != nil {
+		return code.ErrorRedisDeleteFailed
 	}
 	return code.SUCCESS
 }
